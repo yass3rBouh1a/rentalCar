07979efd7e962b2c7146862f159005027e9a8b8f
yass3rBouh1a <140222243+yass3rBouh1a@users.noreply.github.com>
2023-12-28 13:23:57 +0100

    Add files via upload

---
+++ webProject _LV/views/Assets/main.js
@@ -0,0 +1,53 @@
+function display() {
+    let windowLength = window.innerWidth;
+    let body = document.body;
+        
+    if (windowLength < 1000) {
+        body.style.display = 'none';
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
+
+let currentIndex = 0;
+    const slides = document.querySelector('.slides');
+    const slideItems = document.querySelectorAll('.slide');
+    const totalSlides = slideItems.length;
+
+    function showSlide(index) {
+        if (index < 0) {
+            currentIndex = totalSlides - 1;
+        } else if (index >= totalSlides) {
+            currentIndex = 0;
+        } else {
+            currentIndex = index;
+        }
+
+        slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+    }
+
+    function nextSlide() {
+        showSlide(currentIndex + 1);
+    }
+
+    function prevSlide() {
+        showSlide(currentIndex - 1);
+    }
+
+    setInterval(nextSlide,3000); 
